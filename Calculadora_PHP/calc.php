1025fd0b41e3552b11463e4b84a882e70d130254
BBranches <rammos.barbara@gmail.com>
2021-04-15 00:48:37 -0300

    validação e correções

--- Calculadora_PHP/calc.php
+++ Calculadora_PHP/calc.php
@@ -6,39 +6,64 @@ if ($_SERVER["REQUEST_METHOD"]  == "POST") {
   
   $aValido = 0;
   $bValido = 0;
+  $opValido = 0;
+  $sinal = "";
 
   $a=$_POST["a"];
   $b=$_POST["b"];
   $op=$_POST["operacao"];
 
-  if ($a != "") {
+  if (ctype_digit($a)) {
     $aValido = 1;
   } else {
-    echo "Preencha o primeiro número<br><br>";
+      echo "Preencha o primeiro número<br><br>";
   }
-   if ($b != "") {
+   if (ctype_digit($b)) {
     $bValido = 1;
   } else {
-    echo "Preencha o segundo número<br><br>";
+      echo "Preencha o segundo número<br><br>";
   }
-  if ($aValido != 1 or $bValido != 1) {
+  if(!ctype_alpha($op)) {
+    echo "Escolha um operador<br><br>";
+  } else {
+      $opValido = 1;
+  }
+
+  function soma (int $num1, int $num2) {
+    return $num1 + $num2;
+  }
+  function subtracao (int $num1, int $num2) {
+    return $num1 - $num2;
+  }
+  function multiplicacao (int $num1, int $num2) {
+    return $num1 * $num2;
+  }
+  function divisao (int $num1, int $num2) {
+    return $num1 / $num2;
+  }
+
+  if ($aValido != 1 || $bValido != 1 || $opValido != 1) {
     echo "<p>Formulário com erro, preencha novamente</p>";
-    } else {
-        if($op=="Soma") {
-          $result = $a + $b;
-        } elseif($op=="Subtração") {
-          $result = $a - $b;
-        } elseif($op=="Divisão") {
+  } else {
+      if($op=="soma") {
+        $result = soma($a, $b);
+        $sinal = "+";
+      } elseif($op=="sub") {
+          $result = subtracao($a, $b);
+          $sinal = "-";
+      } elseif($op=="div") {
           if($b==0) {
             $result = "inexistente";
+            $sinal = "/";
+          } else {
+              $result = divisao($a, $b);
+              $sinal = "/";
           }
-          else {
-            $result = $a / $b;
-          }
-        } else {
-          $result = $a * $b;
-        }
-        echo "O resultado da $op entre $a e $b é $result.";
-    }   
+      } else {
+          $result = multiplicacao($a, $b);
+          $sinal = "*";
+      }
+      echo "O resultado de $a $sinal $b = $result.";
+  }   
 } 
 ?>
