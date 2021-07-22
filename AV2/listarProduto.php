de048677e829d8a63a11398567706ee7c7a83615
BBranches <rammos.barbara@gmail.com>
2021-07-22 19:15:29 -0300

    Alterações listar

--- AV2/listarProduto.php
+++ AV2/listarProduto.php
@@ -6,9 +6,9 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         
     $result = $conn->query($sql);
 
-    if($result) {
+    if($result->num_rows > 0) {
       echo "<table>";
-      echo "<caption><h1>Produtos</h1></caption>";
+      echo "<caption><h1>Tabela de Produtos</h1></caption>";
       echo "<thead>";
       echo "<tr>";
       echo "<th>Código de Barras</th><th>Nome</th><th>Categoria</th><th>Preço</th><th>Estoque</th>";
@@ -16,36 +16,36 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
       echo "</thead>";
       echo "<tbody>";
     
-    while ($produto = $result->fetch_assoc()) {
-      $id = $produto["id"];
-      $codigoBarra = $produto["codigobarra"];
-      $nome = $produto["nome"];
-      $fabricante = $produto["fabricante"];
-      $categoria = $produto["categoria"];
-      $tipo = $produto["tipo"];
-      $preco = $produto["preco"];
-      $qt = $produto["qt"];
-      $peso = $produto["peso"];
-      $descricao = $produto["descricao"];
-      $linkImg = $produto["linkimg"];
-      $data = $produto["data"];
-      $ativo = $produto["ativo"];
-    
-      $alterar = "./alterarProduto.html?id={$id}" . "&codigobarra=${codigoBarra}" . "&nome=${nome}" . "&fabricante=${fabricante}" . "&categoria=${categoria}" . "&tipo=${tipo}" . "&preco=${preco}" . "&qt=${qt}" . "&peso=${peso}" . "&descricao=${descricao}" . "&linkimg=${linkImg}" . "&data=${data}" . "&ativo=${ativo}";
+      while ($produto = $result->fetch_assoc()) {
+        $id = $produto["id"];
+        $codigoBarra = $produto["codigobarra"];
+        $nome = $produto["nome"];
+        $fabricante = $produto["fabricante"];
+        $categoria = $produto["categoria"];
+        $tipo = $produto["tipo"];
+        $preco = $produto["preco"];
+        $qt = $produto["qt"];
+        $peso = $produto["peso"];
+        $descricao = $produto["descricao"];
+        $linkImg = $produto["linkimg"];
+        $data = $produto["data"];
+        $ativo = $produto["ativo"];
+      
+        $alterar = "./alterarProduto.html?id={$id}" . "&codigobarra=${codigoBarra}" . "&nome=${nome}" . "&fabricante=${fabricante}" . "&categoria=${categoria}" . "&tipo=${tipo}" . "&preco=${preco}" . "&qt=${qt}" . "&peso=${peso}" . "&descricao=${descricao}" . "&linkimg=${linkImg}" . "&data=${data}" . "&ativo=${ativo}";
 
-      $detalhes = "./detalhesProduto.php?codigobarra=${codigoBarra}";
-        
-      if ($produto["ativo"] != 0) {
-        echo "<tr>";
-        echo "<td><span>{$codigoBarra}</span></td>";
-        echo "<td><span><a class='detalhes' href=\"{$detalhes}\">{$nome}</a></span></td>";
-        echo "<td><span>{$categoria}</span></td>";
-        echo "<td><span>R$ {$preco}</span></td>";
-        echo "<td><span>{$qt}</span></td>";
-        echo "<td><span class='table'><a href=\"{$alterar}\"><button>Alterar</button></a></span></td>";
-        echo "</tr>";
+        $detalhes = "./detalhesProduto.php?codigobarra=${codigoBarra}";
+          
+        if ($produto["ativo"] != 0) {
+          echo "<tr>";
+          echo "<td><span>{$codigoBarra}</span></td>";
+          echo "<td><span><a class='detalhes' href=\"{$detalhes}\">{$nome}</a></span></td>";
+          echo "<td><span>{$categoria}</span></td>";
+          echo "<td><span>R$ {$preco}</span></td>";
+          echo "<td><span>{$qt}</span></td>";
+          echo "<td><span class='table'><a href=\"{$alterar}\"><button>Alterar</button></a></span></td>";
+          echo "</tr>";
+        }
       }
-    }
       echo "</tbody>";
       echo "</table>";   
     } else {
--- AV2/listarProdutoCod.php
+++ AV2/listarProdutoCod.php
@@ -30,47 +30,44 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
       $result = $conn->query($sql);
 
       if($result) {
-        echo "<table>";
-        echo "<caption><h1>Produtos</h1></caption>";
-        echo "<thead>";
-        echo "<tr>";
-        echo "<th>Código de Barras</th><th>Nome</th><th>Categoria</th><th>Preço</th><th>Estoque</th>";
-        echo "</tr>";
-        echo "</thead>";
-        echo "<tbody>";
-      
         $produto = $result->fetch_assoc();
-          $id = $produto["id"];
-          $nome = $produto["nome"];
-          $fabricante = $produto["fabricante"];
-          $categoria = $produto["categoria"];
-          $tipo = $produto["tipo"];
-          $preco = $produto["preco"];
-          $qt = $produto["qt"];
-          $peso = $produto["peso"];
-          $descricao = $produto["descricao"];
-          $linkImg = $produto["linkimg"];
-          $data = $produto["data"];
-          $ativo = $produto["ativo"];
+        $id = $produto["id"];
+        $nome = $produto["nome"];
+        $fabricante = $produto["fabricante"];
+        $categoria = $produto["categoria"];
+        $tipo = $produto["tipo"];
+        $preco = $produto["preco"];
+        $qt = $produto["qt"];
+        $peso = $produto["peso"];
+        $descricao = $produto["descricao"];
+        $linkImg = $produto["linkimg"];
+        $data = $produto["data"];
+        $ativo = $produto["ativo"];
         
-          $alterar = "./alterarProduto.html?id={$id}" . "&codigobarra=${codigoBarra}" . "&nome=${nome}" . "&fabricante=${fabricante}" . "&categoria=${categoria}" . "&tipo=${tipo}" . "&preco=${preco}" . "&qt=${qt}" . "&peso=${peso}" . "&descricao=${descricao}" . "&linkimg=${linkImg}" . "&data=${data}" . "&ativo=${ativo}";
+        $alterar = "./alterarProduto.html?id={$id}" . "&codigobarra=${codigoBarra}" . "&nome=${nome}" . "&fabricante=${fabricante}" . "&categoria=${categoria}" . "&tipo=${tipo}" . "&preco=${preco}" . "&qt=${qt}" . "&peso=${peso}" . "&descricao=${descricao}" . "&linkimg=${linkImg}" . "&data=${data}" . "&ativo=${ativo}";
 
-          $detalhes = "./detalhesProduto.php?codigobarra=${codigoBarra}";
+        $detalhes = "./detalhesProduto.php?codigobarra=${codigoBarra}";
             
-          if ($produto["ativo"] != 0) {
-            echo "<tr>";
-            echo "<td><span>{$codigoBarra}</span></td>";
-            echo "<td><span><a class='detalhes' href=\"{$detalhes}\">{$nome}</a></span></td>";
-            echo "<td><span>{$categoria}</span></td>";
-            echo "<td><span>R$ {$preco}</span></td>";
-            echo "<td><span>{$qt}</span></td>";
-            echo "<td><span class='table'><a href=\"{$alterar}\"><button>Alterar</button></a></span></td>";
-            echo "</tr>";
-          } else {
-              echo "Produto Inativo.";
-            }
-          echo "</tbody>";
+        if ($produto["ativo"] != 0) {
+          echo "<table>";
+          echo "<caption><h1>Tabela de Produtos</h1></caption>";
+          echo "<thead>";
+          echo "<tr>";
+          echo "<th>Código de Barras</th><th>Nome</th><th>Categoria</th><th>Preço</th><th>Estoque</th>";
+          echo "</tr>";
+          echo "</thead>";
+          echo "<tr>";
+          echo "<td><span>{$codigoBarra}</span></td>";
+          echo "<td><span><a class='detalhes' href=\"{$detalhes}\">{$nome}</a></span></td>";
+          echo "<td><span>{$categoria}</span></td>";
+          echo "<td><span>R$ {$preco}</span></td>";
+          echo "<td><span>{$qt}</span></td>";
+          echo "<td><span class='table'><a href=\"{$alterar}\"><button>Alterar</button></a></span></td>";
+          echo "</tr>"; 
           echo "</table>";   
+        } else {
+            echo "Produto Inativo.";
+          }
       } else {
           echo "Erro ao buscar produto.";
         }
